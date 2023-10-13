ccff008bc2ec54fed532eb737edad5e78b988311
tspark1077 <147698192+tspark1077@users.noreply.github.com>
2023-10-13 10:11:11 +0900

    Delete ROSSimulator directory

--- ROSSimulator/launch/shelltemp.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-source /root/tesla/models/aws-robomaker-small-warehouse-world-ros1/install/setup.sh
-roslaunch /home/tesla/Desktop/tesla_work/DockerTest/code/ROSSimulator/launch/20231005_180513.launch
