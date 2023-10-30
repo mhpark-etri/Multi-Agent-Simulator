7261ca8c92e44cbafd6c9252c772cbac9eac9791
etri-clara2 <147488330+etri-clara2@users.noreply.github.com>
2023-10-30 10:58:27 +0900

    Update main.py

--- task_manager/main.py
+++ task_manager/main.py
@@ -7,3 +7,9 @@ upt = Point(150,168)
 
 tf_lpt = transform_point(lpt, scale=scale)
 tf_upt = transform_point(upt,  scale=scale)
+
+
+x_init = tf_lpt.x
+y_init = tf_lpt.y
+width =  tf_lpt.x  - tf_upt.x + 1
+inc = 2
