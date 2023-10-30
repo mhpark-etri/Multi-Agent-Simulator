dd572f281228d091d32c96b1f29d543937b8830c
etri-clara2 <147488330+etri-clara2@users.noreply.github.com>
2023-10-30 10:48:20 +0900

    Update area_split.py

--- task_manager/area_split.py
+++ task_manager/area_split.py
@@ -1,1 +1,12 @@
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+import numpy as np
+
+
+os.chdir('d:/temp')
+
+mapfile = 'map.pgm'
+with open(mapfile, 'rb') as imap:
+    gmap = plt.imread(imap)
