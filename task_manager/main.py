baebae54d2aedeb87656bda0a9df204b5d919a80
etri-clara2 <147488330+etri-clara2@users.noreply.github.com>
2023-10-30 10:58:01 +0900

    Update main.py

--- task_manager/main.py
+++ task_manager/main.py
@@ -1,3 +1,9 @@
 ### main ###
 scale = 10
 new_map = transform_map(gmap, pixels = scale)
+
+lpt = Point(220, 220)
+upt = Point(150,168)
+
+tf_lpt = transform_point(lpt, scale=scale)
+tf_upt = transform_point(upt,  scale=scale)
