0b3b850df45b0bc0da2d28a5a7e43fb40e185483
etri-clara2 <147488330+etri-clara2@users.noreply.github.com>
2023-10-30 10:54:07 +0900

    Update area_split.py

--- task_manager/area_split.py
+++ task_manager/area_split.py
@@ -9,3 +9,15 @@ os.chdir('d:/temp')
 mapfile = 'map.pgm'
 with open(mapfile, 'rb') as imap:
     gmap = plt.imread(imap)
+
+
+
+def transform_map(gmap, pixels=10):
+  rows = int(np.ceil(gmap.shape[0] / pixels))
+  cols = int(np.ceil(gmap.shape[1] / pixels)) 
+  new_map = np.zeros((rows, cols), dtype=np.uint8)
+  for i in range(0, rows):
+      for j in range(0, cols):
+        new_map[i, j] = gmap[i*pixels:i*pixels+pixels, j*pixels:j*pixels+pixels].mean()
+
+  return new_map
