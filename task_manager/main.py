c0746904a3e9680365903b73731a7655c9321e27
etri-clara2 <147488330+etri-clara2@users.noreply.github.com>
2023-10-30 10:58:57 +0900

    Update main.py

--- task_manager/main.py
+++ task_manager/main.py
@@ -12,3 +12,15 @@ x_init = tf_lpt.x
 y_init = tf_lpt.y
 width =  tf_lpt.x  - tf_upt.x + 1
 inc = 2
+
+goal_list = []
+for y_cnt in range(0, width, inc):
+  if y_cnt % 2==0: x_init = tf_lpt.x
+  else: x_init = tf_lpt.x - width + 1
+
+  for x_cnt in range(0, width, inc):
+    if y_cnt % 2==0: x = x_init - x_cnt
+    else: x = x_init +x_cnt
+    y= y_init - y_cnt
+    goal_list.append((x,y)) 
+
