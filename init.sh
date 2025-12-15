ce7d99acf09bf68e2f89a0ada91e5ca606e130df
tspark <tspark@teslasystem.co.kr>
2025-12-15 15:03:40 +0900

    init.sh 미사용 설치 제거

--- init.sh
+++ init.sh
@@ -3,36 +3,6 @@
 cd /etc/default/
 echo 'LANG=en_US.UTF-8' | sudo tee -a /etc/default/locale
 
-## AWS Robomaker - AWS 모델들은 당분간 사용하지 않음 ##
-# warehouse build
-# cd /root/tesla/models_aws/aws-robomaker-small-warehouse-world-ros1/
-# rosdep install --from-paths . --ignore-src -r -y
-# colcon build
-# source install/setup.sh
-
-# hospital
-# cd /root/tesla/models_aws/aws-robomaker-hospital-world-ros1/
-# rosws update
-# rosdep install --from-paths . --ignore-src -r -y
-# chmod +x setup.sh
-# ./setup.sh
-# colcon build
-# source install/setup.sh
-
-# small house
-# cd /root/tesla/models_aws/aws-robomaker-small-house-world-ros1/
-# rosws update
-# rosdep install --from-paths . --ignore-src -r -y
-# colcon build
-# source install/setup.sh
-
-# book store
-# cd /root/tesla/models_aws/aws-robomaker-bookstore-world-ros1/
-# rosws update
-# rosdep install --from-paths . --ignore-src -r -y
-# colcon build
-# source install/setup.sh
-
 ## Jnp 복사(개별 빌드 필요 - 페이지 설치 메뉴얼 참고)
 sudo mkdir -p /root/catkin_ws_jnp/src/jnp
 cd /root/catkin_ws_jnp/src/jnp
